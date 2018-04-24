aa890c3c922d9d2e21ade14204f39a9eec7260b1
hudan <996810472@qq.com>
2018-04-24 17:30:59 +0800

    add

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session')
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -17,18 +18,29 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("hdhdhd"));
+app.use(session({
+  secret: 'hdhdhd',
+  resave: false,
+  saveUninitialized: true,
+  expires: 60 * 60 * 1000, //过期时间1小时
+}))
+
+
+
+//静态文件服务
 app.use(express.static(path.join(__dirname, 'web')));
 app.use('/img',express.static(path.join(__dirname, 'web/static/img')));
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Credentials','true');
-    next();
-};
-app.use(allowCrossDomain);
+//cors，资源共享
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     res.header('Access-Control-Allow-Credentials','true');
+//     next();
+// };
+// app.use(allowCrossDomain);
 
 
 app.use('/', indexRouter);
@@ -38,7 +50,7 @@ app.use('/register', registerRouter);
 
 //默认跳到首页
 app.get('/',function(req,res) {
-	 res.redirect(302, '/index.html') //相对当前url的根
+	res.redirect(302, '/index.html') //相对当前url的根
 })
 
 // catch 404 and forward to error handler
@@ -48,13 +60,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	res.redirect(302, '/noPage.html') //相对当前url的根
 });
 
 module.exports = app;
--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,34 @@
 var express = require('express');
+// const cookieParse = require('cookie-parser')
 var router = express.Router();
 var user_bus=require('../db/user_bus')
+var infoCode=require('../public/js/infoCode')
 var crypto = require('crypto')
 
 
 router.get('/',function(req, res, next) {
-	res.render('login');
+	console.log("11111111111",req.session.users,req.signedCookies)
+	var user=req.session.users? req.session.users[req.signedCookies.session_id] : null
+	if(user){
+		res.json({user})
+	}
+	else{
+		res.json({})
+	}
+})
+
+router.get('/out', function(req, res, next) {
+	try{
+		console.log("登出",req.session.users,req.signedCookies.session_id)
+		  if (req.session.users && req.session.users[req.signedCookies.session_id]) {
+		    delete req.session.users[req.signedCookies.session_id]
+		  }
+		  res.clearCookie('session_id')
+		  res.json({msg:infoCode["S0002"]})
+	}
+	catch(e){
+		console.log(e)
+	}
 })
 
 router.post('/', (async function(req, res, next){
@@ -16,29 +39,38 @@ router.post('/', (async function(req, res, next){
 	try{
 		var user=await user_bus.findOne(obj)
 		if(user){
-			//res.render('login',{msg:"登录成功"});
-			console.log("登录成功:"+user)
-			msg="登录成功"
+			msg=infoCode["S0001"]
+			//console.log(msg+user,user._id)
+			res.cookie("session_id", user._id, {
+		      signed: true,
+		      httpOnly: true,
+		      expires: new Date(Date.now() + 3600000),//过期时间为1个小时
+		      // domain: 'api.iamabj.club',
+		    })
+		    if(!req.session){
+		    	req.session={}
+		    }
+		    if (!req.session.users) {
+		      req.session.users = {}
+		    }
+		    req.session.users[user._id] = user
+		    console.log("登录",req.session.users)
 		}
 		else{
-			var user=await user_bus.findByName(req.body.name)
+		    user=await user_bus.findByName(req.body.name)
 			if(user){
-				//res.render('login',{err:"密码错误"});
-				console.log("密码错误")
-				err="密码错误"
+				err=infoCode["F0001"]
 			}
 			else{
-				//res.render('login',{err:"账号不存在"});
-				console.log("账号不存在")
-				err="账号不存在"
+				err=infoCode["F0002"]
 			}
 		}
 		res.json({user,msg,err})
 	}
-	catch(e){
-		//res.render('login',{e});
-		console.log(e)
+	catch(err){
+		res.json({err})
+		console.log(err)
 	}
-}));
+}))
 
 module.exports = router;
