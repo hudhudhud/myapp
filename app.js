8e9da92037ff4674c4a655b55030cd2edb529c95
hudan <996810472@qq.com>
2018-04-27 17:46:38 +0800

    add

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use('/img',express.static(path.join(__dirname, 'web/static/img')));
 
 //cors，资源共享
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8083');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials','true');
